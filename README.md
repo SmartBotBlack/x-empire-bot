d211feb55599731995c879ac6419432609b24bc1
Roman Arestovich <eedoov@gmail.com>
2024-08-09 23:31:00 +0300

    feat: init

---
+++ README.md
@@ -0,0 +1,69 @@
+# X Empire Telegram Game Bot
+
+This repository contains scripts for automating the X Empire game on Telegram using Tampermonkey. The scripts are developed by the SmartBot Team and are designed to run the game in your browser and automate the clicking process to collect points.
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Scripts](#scripts)
+- [Contributing](#contributing)
+
+## Installation
+
+To get started with the TapSport Telegram Game Bot, follow these steps:
+
+### Step 1: Install Tampermonkey
+
+Tampermonkey is a popular userscript manager that is available for various browsers. Install Tampermonkey for your browser from the following link:
+
+- [tampermonkey.net](https://www.tampermonkey.net/)
+
+### Step 2: Install the Scripts
+
+1. **telegram.user.js**: This script enables the game to run in your browser.
+
+   - Click the following link to install the script: [Install telegram.user.js](https://raw.githubusercontent.com/SmartBotBlack/telegram-web-fix-for-open-app/main/src/telegram.user.js)
+   - Tampermonkey will open and prompt you to install the script. Click `Install`.
+
+2. **x-empire.user.js**: This is the main bot script that automates the game.
+
+   - Click the following link to install the script: [Install x-empire.user.js](https://raw.githubusercontent.com/SmartBotBlack/x-empire-bot/main/src/x-empire.user.js)
+   - Tampermonkey will open and prompt you to install the script. Click `Install`.
+
+## Usage
+
+After installing the scripts, follow these steps to start using the bot:
+
+1. Open Telegram Web in your browser.
+2. Navigate to the X Empire game.
+3. Start the bot in your browser.
+4. The bot will begin clicking on the Musk and collecting points.
+
+## Scripts
+
+### telegram.user.js
+
+This script is necessary to initiate the Mini App in your browser. It ensures that the game runs correctly and interacts with the browser environment.
+
+### src/x-empire.user.js
+
+This script automates the gameplay by clicking on images and collecting points. It simulates user actions to enhance your gaming experience.
+
+## Contributing
+
+We welcome contributions to improve the X Empire Telegram Game Bot. If you have any suggestions or encounter any issues, please open an issue or submit a pull request.
+
+1. Fork the repository.
+2. Create a new branch (`git checkout -b feature-branch`).
+3. Make your changes.
+4. Commit your changes (`git commit -m 'Add new feature'`).
+5. Push to the branch (`git push origin feature-branch`).
+6. Open a pull request.
+
+## Disclaimer
+
+This script is provided by the SmartBot Team and is intended for educational and personal use only. Use it at your own risk.
+
+---
+
+For more information, visit [SmartBot Team](https://smartbot.black/).
---
+++ src/x-empire.user.js
@@ -0,0 +1,109 @@
+// ==UserScript==
+// @name         X Empire [SmartBot]
+// @namespace    https://smartbot.black/
+// @version      1.0.0
+// @description  Bot for playing x-empire in telegram
+// @author       Smartbot Team
+// @match        https://game.muskempire.io/*
+// @icon         https://game.muskempire.io/favicon-32x32.png
+// @grant        none
+// ==/UserScript==
+
+(async () => {
+	const fakeUserAgent =
+		"Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1";
+
+	const fakeUserAgentData = {
+		brands: [
+			{ brand: "Apple", version: "14" },
+			{ brand: "Safari", version: "604.1" },
+		],
+		mobile: true,
+		platform: "iOS",
+	};
+
+	Object.defineProperty(navigator, "userAgent", {
+		get: () => fakeUserAgent,
+	});
+
+	Object.defineProperty(navigator, "userAgentData", {
+		get: () => fakeUserAgentData,
+	});
+
+	Object.defineProperty(navigator, "platform", {
+		get: () => "iPhone",
+	});
+
+	const getEnergy = () =>
+		Number.parseInt(
+			document
+				.querySelector(".battery")
+				?.nextSibling?.innerText?.replace(",", "") ?? 1000,
+		);
+
+	const getRandomInt = (min, max) =>
+		Math.floor(Math.random() * (max - min + 1)) + min;
+
+	const emulateMobileTapOnImage = async (image) => {
+		const rect = image.getBoundingClientRect();
+
+		const x = Math.floor(Math.random() * rect.width) + rect.left;
+		const y = Math.floor(Math.random() * rect.height) + rect.top;
+		console.log("x,y", x, y);
+
+		const pointerdownEvent = new PointerEvent("pointerdown", {
+			bubbles: true,
+			cancelable: true,
+			pointerId: Date.now(),
+			clientX: x,
+			clientY: y,
+			pointerType: "touch",
+			pressure: 0.5,
+			isPrimary: true,
+		});
+
+		const pointermoveEvent = new PointerEvent("pointermove", {
+			bubbles: true,
+			cancelable: true,
+			pointerId: Date.now(),
+			clientX: x,
+			clientY: y,
+			pointerType: "touch",
+			pressure: 0.5,
+			isPrimary: true,
+		});
+
+		const pointerupEvent = new PointerEvent("pointerup", {
+			bubbles: true,
+			cancelable: true,
+			pointerId: Date.now(),
+			clientX: x,
+			clientY: y,
+			pointerType: "touch",
+			pressure: 0,
+			isPrimary: true,
+		});
+
+		image.dispatchEvent(pointerdownEvent);
+		await new Promise((res) => setTimeout(res, getRandomInt(10, 500)));
+		image.dispatchEvent(pointermoveEvent);
+		image.dispatchEvent(pointerupEvent);
+	};
+
+	while (true) {
+		try {
+			const currentEnergy = getEnergy();
+
+			if (currentEnergy > getRandomInt(10, 200)) {
+				await emulateMobileTapOnImage(document.querySelector("#oreol"));
+			} else {
+				await new Promise((res) =>
+					setTimeout(res, getRandomInt(1, 20) * 60 * 1e3),
+				);
+			}
+		} catch (error) {
+			console.error(error);
+			await new Promise((res) => setTimeout(res, 1e4));
+		}
+	}
+})();
